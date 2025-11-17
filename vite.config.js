179f6cab23754f1a37762fef5c265c0bc05ea8fc
Diana Yesenia Campos Tenorio <yesenia.campos@zorya.mx>
2025-11-17 02:56:05 -0600

    add build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-  ],
-})
+  base: "/yes-tom-boda/",   // <--- NOMBRE DE TU REPO EXACTO
+  plugins: [react()],
+});
